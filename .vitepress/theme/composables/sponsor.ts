b941afb0a6f525babfbed2032fd18a3dc4445847
牟勇 <my24251325@gmail.com>
2023-01-01 04:01:28 +0800

    更新项目依赖信息

--- .vitepress/theme/composables/sponsor.ts
+++ .vitepress/theme/composables/sponsor.ts
@@ -18,7 +18,7 @@ interface Sponsor {
 // shared data across instances so we load only once.
 const data = ref()
 
-const dataHost = '/vitepress-doc-website'
+const dataHost = 'https://blog.iwnweb.com/vitepress-doc-website'
 const dataUrl = `${dataHost}/assets/sponsor/sponsors.json`
 
 export function useSponsor() {

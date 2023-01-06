70d9c4e2a16290a4240b143dd8764f733eec0b77
mouyong <mouyong@users.noreply.github.com>
2023-01-06 16:02:19 +0000

    Deploying to gh-pages from @ mouyong/vitepress-doc-website@5db7661554584109cbe2b2bedaedd983fb7ffc03 🚀

---
+++ assets/api_response.md.49bd33f1.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as a,c as n,a as o}from"./app.7fbf0733.js";const i=JSON.parse('{"title":"全局接口响应","description":"","frontmatter":{},"headers":[],"relativePath":"api/response.md","lastUpdated":1673020889000}'),l={name:"api/response.md"},p=o("",2),t=[p];function e(c,r,D,F,y,C){return a(),n("div",null,t)}const _=s(l,[["render",e]]);export{i as __pageData,_ as default};

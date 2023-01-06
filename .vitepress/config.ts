5db7661554584109cbe2b2bedaedd983fb7ffc03
牟勇 <my24251325@gmail.com>
2023-01-07 00:01:29 +0800

    更新链接

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   },
 
   themeConfig: {
-    // example: logo: '/logo.png',
+    // example: logo: '/logo_square_en.png',
     // example: logo: false,
     logo: './logo_square_en.jpeg',
     siteTitle: undefined,
@@ -47,10 +47,23 @@ export default defineConfig({
       '/contributing/': sidebarCommunity(),
     },
     /** https://vitepress.vuejs.org/guide/theme-nav.html#social-links */
-    socialLinks: [{ icon: 'github', link: 'https://github.com/mouyong/vitepress-doc-website' }],
+    socialLinks: [
+      {
+        icon: {
+          svg: '<svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="1894" height="128" width="128"><path d="M512 1024C230.4 1024 0 793.6 0 512S230.4 0 512 0s512 230.4 512 512-230.4 512-512 512z m259.2-569.6H480c-12.8 0-25.6 12.8-25.6 25.6v64c0 12.8 12.8 25.6 25.6 25.6h176c12.8 0 25.6 12.8 25.6 25.6v12.8c0 41.6-35.2 76.8-76.8 76.8h-240c-12.8 0-25.6-12.8-25.6-25.6V416c0-41.6 35.2-76.8 76.8-76.8h355.2c12.8 0 25.6-12.8 25.6-25.6v-64c0-12.8-12.8-25.6-25.6-25.6H416c-105.6 0-188.8 86.4-188.8 188.8V768c0 12.8 12.8 25.6 25.6 25.6h374.4c92.8 0 169.6-76.8 169.6-169.6v-144c0-12.8-12.8-25.6-25.6-25.6z" fill="#888888" p-id="1895"></path></svg>'
+        },
+        link: 'https://github.com/mouyong/vitepress-doc-website' 
+      },
+      {
+        icon: {
+          svg: '<svg class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4808" height="128" width="128"><path d="M832 160c16.954 0 32.986 6.696 45.145 18.855C889.304 191.014 896 207.046 896 224v416c0 16.954-6.696 32.986-18.855 45.145C864.986 697.304 848.954 704 832 704H192c-16.954 0-32.986-6.696-45.145-18.855C134.696 672.986 128 656.954 128 640V224c0-16.954 6.696-32.986 18.855-45.145C159.014 166.696 175.046 160 192 160h640m0-64H192c-70.4 0-128 57.6-128 128v416c0 70.4 57.6 128 128 128h640c70.4 0 128-57.6 128-128V224c0-70.4-57.6-128-128-128zM736 928H288c-17.6 0-32-14.4-32-32s14.4-32 32-32h448c17.6 0 32 14.4 32 32s-14.4 32-32 32zM736 352H288c-17.6 0-32-14.4-32-32s14.4-32 32-32h448c17.6 0 32 14.4 32 32s-14.4 32-32 32zM544 576H288c-17.6 0-32-14.4-32-32s14.4-32 32-32h256c17.6 0 32 14.4 32 32s-14.4 32-32 32z" p-id="4809"></path></svg>'
+        },
+        link: 'https://laravel-workerman.iwnweb.com/' 
+      },
+    ],
     footer: {
       message: 'Released under the <a href="https://github.com/vuejs/vitepress/blob/main/LICENSE">MIT License</a>.',
-      copyright: 'Copyright © 2014-present <a href="https://github.com/mouyong">mouyong</a>'
+      copyright: 'Copyright © 2014-present <a href="https://blog.iwnweb.com">mouyong</a>'
     },
 
     editLink: {
@@ -117,7 +130,6 @@ export default defineConfig({
     }
   }
 })
-
 
 function navbar() {
   return [

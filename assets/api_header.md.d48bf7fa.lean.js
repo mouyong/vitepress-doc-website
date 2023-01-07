29164fcf51218f1f2f188be82b175d96885d1130
mouyong <mouyong@users.noreply.github.com>
2023-01-07 11:33:26 +0000

    Deploying to gh-pages from @ mouyong/vitepress-doc-website@88f579efe34f7eeaa24533cd063ec10b63ed3682 🚀

--- assets/api_header.md.d48bf7fa.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,o as e,c as a,a as d}from"./app.a9523ec5.js";const m=JSON.parse('{"title":"全局请求头","description":"","frontmatter":{},"headers":[],"relativePath":"api/header.md","lastUpdated":1673090887000}'),r={name:"api/header.md"},o=d("",2),n=[o];function c(_,s,h,i,p,l){return e(),a("div",null,n)}const u=t(r,[["render",c]]);export{m as __pageData,u as default};

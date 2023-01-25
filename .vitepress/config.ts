aaac6d03eb98b78aa4f1d31166e5769d0ed0fd97
牟勇 <my24251325@gmail.com>
2023-01-25 19:58:22 +0800

    chore: 更新 sitemap.xml

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -24,8 +24,6 @@ export default defineConfig({
     ['meta', { property: 'og:title', content: '文档站' }],
     ['meta', { property: 'og:description', content: '帮助你快速创建项目的官方文档站点；快速搭建属于你的个人知识体系、博客；' }],
     ['meta', { property: 'og:image', content: './logo_square_en.png' }],
-
-    ['meta', { property: 'twitter:card', content: 'summary_large_image' }],
   ],
 
   markdown: {
